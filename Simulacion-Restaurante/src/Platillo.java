27ca92ef53bc267acd468a99b4ca2f138d5aa95d
Ryan Munoz <aldrich@Ryans-MacBook-Air.local>
2026-05-20 11:28:03 -0700

    hola

--- Simulacion-Restaurante/src/Platillo.java
+++ Simulacion-Restaurante/src/Platillo.java
@@ -6,13 +6,15 @@ public class Platillo {
     private String TerminoCoccion;
     private boolean esVegano;
 
-    
+
     public Platillo(String nombrePlatillo, int precio, int tiempoPreparacion, String terminoCoccion, boolean esVegano) {
         this.nombrePlatillo = nombrePlatillo;
         Precio = precio;
         TiempoPreparacion = tiempoPreparacion;
         TerminoCoccion = terminoCoccion;
         this.esVegano = esVegano;
+
+         // hola
     }
 
     
