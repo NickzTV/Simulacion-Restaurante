ea2182d61af57ed9cce03eb50092a2258eebab06
Nicole <nicole.riverafelix@gmail.com>
2026-05-20 10:58:45 -0700

    Serializacion de Menu y ColadeOrden

--- src/Preparar.java
+++ src/Preparar.java
@@ -1,2 +1,9 @@
 public class Preparar {
+
+    public synchronized void preparando (String nombre, String nombrePlatillo){
+
+        //aqui tendra los pasos para simular la preparacion del platillo
+        //se menciona el nombre del chef, su area de especialidad, y el nombre del platillo.
+
+    }
 }
