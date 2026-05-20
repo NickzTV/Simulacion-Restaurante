37d158ba4ec026a59e3160f88ec8b724ac72889d
ImAldrich <aldrichtheshepherd@gmail.com>
2026-05-20 12:03:56 -0700

    Actualizacion

--- src/Platillo.java
+++ src/Platillo.java
@@ -1,3 +1,19 @@
 public class Platillo {
-    //hola nick 
+    
+
+    private String nombre;
+    private int precio;
+    private String TerminoCoccion;
+    private int TiempoPreparacion; 
+    private boolean esVegano;
+    
+    public Platillo(String nombre, int precio, String terminoCoccion, int tiempoPreparacion, boolean esVegano) {
+        this.nombre = nombre;
+        this.precio = precio;
+        TerminoCoccion = terminoCoccion;
+        TiempoPreparacion = tiempoPreparacion;
+        this.esVegano = esVegano;
+    }
+
+    
 }
