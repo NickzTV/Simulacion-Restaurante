704ae01316685cd48e82757010765fe5d0b7d857
Ryan Munoz <aldrich@Ryans-MacBook-Air.local>
2026-05-20 11:25:39 -0700

    Atributos y constructor clase Platillo

--- Simulacion-Restaurante/src/Platillo.java
+++ Simulacion-Restaurante/src/Platillo.java
@@ -1,2 +1,19 @@
 public class Platillo {
+
+    private String nombrePlatillo;
+    private int Precio;
+    private int TiempoPreparacion;
+    private String TerminoCoccion;
+    private boolean esVegano;
+
+    
+    public Platillo(String nombrePlatillo, int precio, int tiempoPreparacion, String terminoCoccion, boolean esVegano) {
+        this.nombrePlatillo = nombrePlatillo;
+        Precio = precio;
+        TiempoPreparacion = tiempoPreparacion;
+        TerminoCoccion = terminoCoccion;
+        this.esVegano = esVegano;
+    }
+
+    
 }
