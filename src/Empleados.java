0b71ec5d995bec51ee7008090d7014403c033299
Aldrich <aldrichtheshepherd@gmail.com>
2026-05-20 09:01:32 -0700

    Atributos clase Empleado
    cambios :3

--- src/Empleados.java
+++ src/Empleados.java
@@ -1,2 +1,15 @@
 public class Empleados {
+  
+  private String nombre;
+  private int AniosExperiencia;
+  private int edad;
+  private int Id;
+  
+   public Empleados (String nombre, int aniosExperiencia, int edad, int id) {
+    this.nombre = nombre;
+    AniosExperiencia = aniosExperiencia;
+    this.edad = edad;
+    Id = id;
+  }
+
 }
