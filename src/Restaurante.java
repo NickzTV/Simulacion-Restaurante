5776dfbb722667c66d232ab42bccc85971d6a839
Nicole <nicole.riverafelix@gmail.com>
2026-05-20 11:29:25 -0700

    menu y creacion de archivo de txt

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Restaurante {
 
     //este va a ser nuestro main :3
 
+    public static Scanner sc = new Scanner(System.in);
+    public static ArrayList<ColaDeOrden> colaDeOrdenes = new ArrayList<ColaDeOrden>();
+    public static ArrayList<Platillo> Menu = new  ArrayList<Platillo>();
+
+    public static void main(String[] args) {
+        //rutas de los archivos para la serializacion
+        String rutaMenu = "Menu.txt";
+        String  rutaColaDeOrden = "ColaDeOrden.txt";
+
+        //cargar datos existentes (si hay)
+
+    }
 }
