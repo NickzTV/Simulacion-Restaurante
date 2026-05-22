ebf9212f0ac911e69aab187c2d158ba059243cdc
Nicole <nicole.riverafelix@gmail.com>
2026-05-22 11:32:42 -0700

    fix

--- src/Postre.java
+++ src/Postre.java
@@ -1,6 +1,7 @@
 public class Postre extends Platillo{
     private Boolean esVegano;
-    private Boolean esDiabetico
+    private Boolean esDiabetico;
+
 
     public Postre(String nombre, int precio, String descripcion, int tiempoPreparacion, String alergenos, Boolean esVegano, Boolean esDiabetico) {
         super(nombre, precio, descripcion, tiempoPreparacion, alergenos);
