73628f4480acf5c8713f208642469551dc17ec5c
torresandrea <torres.andrea@cetys.edu.mx>
2026-05-25 08:45:10 -0700

    Parte de la serializacion

--- src/Preparar.java
+++ src/Preparar.java
@@ -1,8 +1,15 @@
 public class Preparar {
 
-    public synchronized void preparando (String nombre, String nombrePlatillo){
+    public synchronized void preparando (String nombre, String nombrePlatillo, int tiempoPreparacion){
 
-        
+        System.out.println(nombre+ " esta preparando el platillo "+nombrePlatillo);
+        try{
+            Thread.sleep(tiempoPreparacion);
+        }
+        catch (InterruptedException e){
+            System.out.println(e);
+        }
+        System.out.println()
         //aqui tendra los pasos para simular la preparacion del platillo
         //se menciona el nombre del chef, su area de especialidad, y el nombre del platillo.
         // Utilizar hilos, para poder simular la preparacion del platillo  y dependiendo de la informacion del platillo es lo que se tardara en cocinar dentro del archivo txt.
