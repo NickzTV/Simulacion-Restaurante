bf3a1b5f574bba2fb34fa28f7d8a9e247ef08a30
torresandrea <torres.andrea@cetys.edu.mx>
2026-05-26 00:19:24 -0700

    Cambio en hilos

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Restaurante {
-
     //este va a ser nuestro main :3
 
     public static Scanner sc = new Scanner(System.in);
+
     public static ArrayList<ColaDeOrden> colaDeOrdenes = new ArrayList<ColaDeOrden>();
     public static ArrayList<Platillo> Menu = new  ArrayList<Platillo>();
 
@@ -17,10 +17,18 @@ public class Restaurante {
 
         System.out.println("=====Bienvenido al restaurante labubub======");
 
+        Thread hiloChef = new Thread(new Chef("Ryan", 5, 25, 1));
+        Thread hiloRepostero = new Thread(new Repostero("Andrea", 3, 25, 2));
+        Thread hiloBarista = new Thread(new Barista("Nicole", 4, 25, 3));
+
+        hiloChef.start();
+        hiloRepostero.start();
+        hiloBarista.start();
+
 
         //rutas de los archivos para la serializacion
-        String rutaMenu = "Menu.txt";
-        String  rutaColaDeOrden = "ColaDeOrden.dat";
+        //String rutaMenu = "Menu.txt";
+        //String  rutaColaDeOrden = "ColaDeOrden.dat";
 
         //cargar datos existentes (si hay)
 
@@ -37,18 +45,60 @@ public class Restaurante {
         switch(op){
             case 1: leerMenu("Menu.txt");
             break;
-                
+            case 5:
+                salir = true;
+                break;
         }
-
-
         }
+        System.exit(0);
     }
-
+    
+    public static synchronized ColaDeOrden tomarPlatilloPorEspecialidad(Empleados empleado){
+        for (int i=0; i < colaDeOrdenes.size();i++) {
+            ColaDeOrden orden = colaDeOrdenes.get(i);
+            Platillo platillo = orden.getPlatillo();
+            
+            if (empleado instanceof Chef && platillo instanceof PlatilloFuerte){
+                return colaDeOrdenes.remove(i); //una vez que se inicia el pedido, tambien se elimina de la cola de ordenes
+            }
+            if (empleado instanceof Repostero && platillo instanceof Postre){
+                return colaDeOrdenes.remove(i);
+            }
+            if (empleado instanceof Barista && platillo instanceof Bebida){
+                return colaDeOrdenes.remove(i);
+            }
+        }
+        return null; //si no hay ordenes disponibles para el empleado, se devuelve null
+    }
     private static int leerInt(String string, int i, int j) {
-        throw new UnsupportedOperationException("Unimplemented method 'leerInt'");
-        
+        int opcion;
+        do{
+            System.out.print(string + ": ");
+            while (!sc.hasNextInt()){
+                System.out.println("Entrada no válida. Por favor, ingrese un número entero.");
+                sc.next(); // Limpiar la entrada no válida
+            }
+            opcion = sc.nextInt();
+        }while (opcion < i || opcion > j);
+        return opcion;
     }
+        //throw new UnsupportedOperationException("Unimplemented method 'leerInt'");
+        
+    //}
+    pubLic static void leerMenu(String nombreArchivo){
+        try (BufferedReader lector = new BufferedReader(new FileReader(nombreArchivo))){
 
+            String linea;
+            while ((linea = lector.readLine()) != null){
+                System.out.println(linea);
+            }
+
+        }catch (IOException e){
+            System.err.println("Error al leer el archivo: " + e.getMessage());
+        }
+    }
+}
+/* 
     private static  void MostrarMenu(){
 
     }
@@ -77,19 +127,5 @@ public class Restaurante {
         }catch (IOException e){
             System.err.println("Error al leer el archivo: " + e.getMessage());
         }
-    }
-    public static void main(String[] args) {
-     Scanner scanner = new Scanner(System.in);
-     Preparar preparar = new Preparar();
-     
-     int cantidadPlatillos = 0;
-     Thread[] hilos = new Thread[cantidadPlatillos]; //aqui se debe revisar cada lista de los diferentes tipos de pedidos
-
-
-     for(Thread hilo: hilos){
-        hilos.start();
-     }
-
-
     }
 }
